90212dc97e71e649b538594b5572d9c947435644
Goakiller96 <71164390+Goakiller96@users.noreply.github.com>
2021-07-19 13:09:46 +0500

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,35 +1,69 @@
-<meta http-equiv='refresh' content='3; url=https://you-hands.ru'>
-<meta charset="UTF-8" />
 <?php
 
-	if (isset($_POST['name']) && $_POST['name'] != "")//если существует атрибут NAME и он не пустой то создаем переменную для отправки сообщения
-		$name = $_POST['name'];
-	else die ("Не заполнено поле \"Имя\"");//если же атрибут пустой или не существует то завершаем выполнение скрипта и выдаем ошибку пользователю.
+//Retrieve form data. 
+//GET - user submitted data using AJAX
+//POST - in case user does not support javascript, we'll use POST instead
+$name = ($_GET['name']) ? $_GET['name'] : $_POST['name'];
+$email = ($_GET['email']) ?$_GET['email'] : $_POST['email'];
+$comment = ($_GET['comment']) ?$_GET['comment'] : $_POST['comment'];
 
-	if (isset($_POST['email']) && $_POST['email'] != "") //тут все точно так же как и в предыдущем случае
-		$email = $_POST['email'];
-	else die ("Не заполнено поле \"Email\"");
+//flag to indicate which method it uses. If POST set it to 1
 
-	if (isset($_POST['subjects']) && $_POST['subjects'] != "") 
-		$sub = $_POST['subjects'];
-	else die ("Не заполнено поле \"Тема\"");
+if ($_POST) $post=1;
 
-	if (isset($_POST['message']) && $_POST['message'] != "") 
-		$body = $_POST['message'];
-	else die ("Не заполнено поле \"Сообщение\"");
-	 
+//Simple server side validation for POST data, of course, you should validate the email
+if (!$name) $errors[count($errors)] = 'Please enter your name.';
+if (!$email) $errors[count($errors)] = 'Please enter your email.'; 
+if (!$comment) $errors[count($errors)] = 'Please enter your comment.'; 
+
+//if the errors array is empty, send the mail
+if (!$errors) {
+
+	//recipient - replace your email here
+	$to = 'kiseleffav@gmail.com';	
+	//sender - from the form
+	$from = $name . ' <' . $email . '>';
+	
+	//subject and the html message
+	$subject = 'Message from ' . $name;	
+	$message = 'Name: ' . $name . '<br/><br/>
+		       Email: ' . $email . '<br/><br/>		
+		       Message: ' . nl2br($comment) . '<br/>';
+
+	//send the mail
+	$result = sendmail($to, $subject, $message, $from);
+	
+	//if POST was used, display the message straight away
+	if ($_POST) {
+		if ($result) echo 'Thank you! We have received your message.';
+		else echo 'Sorry, unexpected error. Please try again later';
+		
+	//else if GET was used, return the boolean value so that 
+	//ajax script can react accordingly
+	//1 means success, 0 means failed
+	} else {
+		echo $result;	
+	}
+
+//if the errors array has values
+} else {
+	//display the errors message
+	for ($i=0; $i<count($errors); $i++) echo $errors[$i] . '<br/>';
+	echo '<a href="index.html">Back</a>';
+	exit;
+}
 
 
-	$address = "Promplazma@mail.ru";//адрес куда будет отсылаться сообщение для администратора
-	$mes  = "Имя: $name \n";	//в этих строчках мы заполняем текст сообщения. С помощью оператора .= мы просто дополняем текст в переменную
-	$mes .= "E-mail: $email \n";
- 	$mes .= "Тема: $sub \n";
- 	$mes .= "Текст: $body"; 
-	$send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = UTF-8\r\nFrom:$email");//собственно сам вызов функции отправки сообщения на сервере
+//Simple mail function with HTML header
+function sendmail($to, $subject, $message, $from) {
+	$headers = "MIME-Version: 1.0" . "\r\n";
+	$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
+	$headers .= 'From: ' . $from . "\r\n";
+	
+	$result = mail($to,$subject,$message,$headers);
+	
+	if ($result) return 1;
+	else return 0;
+}
 
-	if ($send) //проверяем, отправилось ли сообщение
-		echo "Сообщение отправлено успешно! Перейти на <a href='https://You-hands.ru/'>you-hands.ru</a>, если вас не перенаправило вручную.";
-	else 
-		echo "Ошибка, сообщение не отправлено! Возможно, проблемы на сервере";
-		 
 ?>
