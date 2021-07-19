5f2286eb08fc9585a88dc94ae0e0a8ab17d0f61d
Goakiller96 <71164390+Goakiller96@users.noreply.github.com>
2021-07-19 13:44:41 +0500

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,69 +1,28 @@
 <?php
-
-//Retrieve form data. 
-//GET - user submitted data using AJAX
-//POST - in case user does not support javascript, we'll use POST instead
-$name = ($_GET['name']) ? $_GET['name'] : $_POST['name'];
-$email = ($_GET['email']) ?$_GET['email'] : $_POST['email'];
-$comment = ($_GET['comment']) ?$_GET['comment'] : $_POST['comment'];
-
-//flag to indicate which method it uses. If POST set it to 1
-
-if ($_POST) $post=1;
-
-//Simple server side validation for POST data, of course, you should validate the email
-if (!$name) $errors[count($errors)] = 'Please enter your name.';
-if (!$email) $errors[count($errors)] = 'Please enter your email.'; 
-if (!$comment) $errors[count($errors)] = 'Please enter your comment.'; 
-
-//if the errors array is empty, send the mail
-if (!$errors) {
-
-	//recipient - replace your email here
-	$to = 'kiseleffav@gmail.com';	
-	//sender - from the form
-	$from = $name . ' <' . $email . '>';
-	
-	//subject and the html message
-	$subject = 'Message from ' . $name;	
-	$message = 'Name: ' . $name . '<br/><br/>
-		       Email: ' . $email . '<br/><br/>		
-		       Message: ' . nl2br($comment) . '<br/>';
-
-	//send the mail
-	$result = sendmail($to, $subject, $message, $from);
-	
-	//if POST was used, display the message straight away
-	if ($_POST) {
-		if ($result) echo 'Thank you! We have received your message.';
-		else echo 'Sorry, unexpected error. Please try again later';
-		
-	//else if GET was used, return the boolean value so that 
-	//ajax script can react accordingly
-	//1 means success, 0 means failed
-	} else {
-		echo $result;	
-	}
-
-//if the errors array has values
+// Обязательно измените e-mail на свой
+$sendto = "promplazma@mail.ru";
+$usermail = $_POST['email'];
+$username = $_POST['name'];
+$userphone = $_POST['phone'];
+$content = nl2br($_POST['msg']);
+// Формирование заголовка письма
+$subject = "Новое сообщение";
+$headers = "From: " . strip_tags($usermail) . "\r\n";
+$headers .= "Reply-To: ". strip_tags($usermail) . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html;charset=utf-8 \r\n";
+// Формирование тела письма
+$msg = "<html-->";
+$msg .= "<h2 style="font-weight:bold">Новое сообщение</h2>\r\n";
+$msg .= "<p><strong>Имя:</strong> ".$username."</p>\r\n";
+$msg .= "<p><strong>Номер телефона:</strong> ".$userphone."</p>\r\n";
+$msg .= "<p><strong>Почта:</strong> ".$usermail."</p>\r\n";
+$msg .= "<p><strong>Сообщение:</strong> ".$content."</p>\r\n";
+$msg .= "";
+// отправка сообщения
+if(@mail($sendto, $subject, $msg, $headers)) {
+	echo "true";
 } else {
-	//display the errors message
-	for ($i=0; $i<count($errors); $i++) echo $errors[$i] . '<br/>';
-	echo '<a href="index.html">Back</a>';
-	exit;
+	echo "false";
 }
-
-
-//Simple mail function with HTML header
-function sendmail($to, $subject, $message, $from) {
-	$headers = "MIME-Version: 1.0" . "\r\n";
-	$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
-	$headers .= 'From: ' . $from . "\r\n";
-	
-	$result = mail($to,$subject,$message,$headers);
-	
-	if ($result) return 1;
-	else return 0;
-}
-
 ?>
