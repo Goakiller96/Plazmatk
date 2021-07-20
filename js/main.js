b5658b5ee1f4c8805d065532f037dd5232938a21
Goakiller96 <71164390+Goakiller96@users.noreply.github.com>
2021-07-20 16:48:57 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -41,3 +41,37 @@ $(function () {
         }
     });
 });
+
+
+
+
+
+
+
+
+$('.menu-btn').on('click', function(e) {
+    e.preventDefault;
+    $(this).toggleClass('menu-btn_active');
+  });
+
+
+
+
+  
+
+// АККОРДЕОН
+
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight){
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
